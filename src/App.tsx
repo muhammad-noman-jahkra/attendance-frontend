16781bc4897db2cd288b33d3d07de4ea45c66cab
Muhammad Noman <muhammad.noman@laksol.com>
2023-10-12 05:06:08 +0500

    Simple Screen to Att Record

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'
 
 function App() {
+  const [attendanceData, setAttendanceData] = useState([]);
+  const [name, setInputValue] = useState('');
+
+  // useEffect(() => {
+    const handleBlur = () => {
+    axios.post('http://127.0.0.1:8000/api/emp-att-record',
+    { emp_name: name }
+    ,{
+      headers: {"Access-Control-Allow-Origin": "*"},      
+    })
+      .then((response) => {
+        // console.log(response.data[0].employee_id);
+        setAttendanceData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Attendance List</h1>
+      Emp Name  
+      <input
+        type="text"
+        value={name}
+        placeholder='Enter Name to search'
+        onChange={(e) => setInputValue(e.target.value)}
+        onBlur={handleBlur} 
+      />
+
+      <table>
+      <tr>
+        <td>Name</td>
+        <td>Check-In</td>
+        <td>Check-Out</td>
+        <td>Total Working Hours</td>
+      </tr>      
+              
+        {attendanceData.map((entry: any) => (
+          <tr key={entry.id}>
+          <td>{entry.name}</td>
+          <td>{entry.In}</td>
+          <td>{entry.Out}</td>
+          <td>{entry.hours}</td>          
+          </tr>
+        ))}
+      
+      </table>
     </div>
   );
 }
